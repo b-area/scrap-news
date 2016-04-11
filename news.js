7a22c9743d5c03f39e5b1d5432adf999fd5f33b0
b-area <b-area@users.noreply.github.com>
2016-04-11 09:00:04 -0500

    Create news.js

---
+++ news.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+var cors = require('cors');
+app.use(cors());
+
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+app.get('/articles', function(req, res){
+	url = 'http://americansongwriter.com/features/articles/';
+	
+	request(url, function(error, response, html)
+	{
+		var news = [];
+		
+		if(!error){
+			var $ = cheerio.load(html);
+			$posts = $('.home-roll').children();
+
+			var index = 0;
+			$posts.each(function(i, elem) {
+				
+				var title, link, image, source;
+				var json  = { title : "", link : "", image: "", source: ""};
+				
+				json.link 	= "http://americansongwriter.com" + $(this).find('a').attr('href');
+				json.image  = $(this).find('img').attr('src');
+				json.title  =  $(this).find('h4').text(); /*$(this).find('.blog-roll-footer').text();*/
+				json.source = "www.americansongwriter.com";
+				
+				//console.log(json);
+				
+				if (json.title !== "Sponsor" && json.title !== ""){
+					news[index] = json; 
+					index = index + 1;
+				}
+			})
+		}
+		
+		news.sort( function() { return 0.5 - Math.random() } );
+		var valiny = {"articles": news.slice(0, 6)}
+		res.send(valiny);
+	})
+})
+
+
+app.listen('3030')
+console.log('Magic happens on port 3030');
+exports = module.exports = app;
